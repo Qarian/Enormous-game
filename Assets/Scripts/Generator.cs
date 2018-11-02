7c31348488353fa56cd4d77880360d65657b973d
Qarian <qarian25@gmail.com>
2018-11-02 18:03:33 +0100

    Colors in generator

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -4,11 +4,20 @@ public class Generator : MonoBehaviour {
 
     public GameObject[] obstackles;
     public GameObject CombinationStartPrefab;
+
+    [Space]
+    public Color[] colors;
+    public bool randomColors = false;
+    public int combinationsForColor = 10;
+    int colorId;
+    int leftCombinationsForColor;
+
     [Space]
     public Combination[] levels;
     public float startPosition = 0f;
     [SerializeField]
     int combinationsLength = 3;
+
     public bool GenerateOnStart = true;
 
     Transform[] combinations;
@@ -25,6 +34,8 @@ public class Generator : MonoBehaviour {
 
     void Start()
     {
+        colorId = colors.Length - 1;
+        NewColor();
 
         combinations = new Transform[combinationsLength];
         if (GenerateOnStart)
@@ -50,18 +61,48 @@ public class Generator : MonoBehaviour {
     {
         combinations[number] = Instantiate(CombinationStartPrefab, Vector3.zero, Quaternion.identity, transform).transform;
 
+        Color color = GetColor();
+
         int id = Random.Range(0, levels.Length);
-        float newDistance = 0f;
+        
         for (int i = 0; i < levels[id].elements.Length; i++)
         {
-            newDistance = levels[id].elements[i].positionZ;
             Vector3 pos = new Vector3(0, 0, levels[id].elements[i].positionZ);
             Quaternion rotation = levels[id].elements[i].rotation;
             Blockade blockade = Instantiate(obstackles[levels[id].elements[i].prefab], pos, rotation, combinations[number]).GetComponent<Blockade>();
             blockade.speed = levels[id].elements[i].speed;
+            blockade.gameObject.GetComponent<Renderer>().material.color = color;
         }
-
+        float newDistance = levels[id].elements[levels[id].elements.Length - 1].positionZ;
         combinations[number].Translate(0, 0, startPosition);
         startPosition += newDistance + levels[id].freeSpace;
+    }
+
+    Color GetColor()
+    {
+        Color ret = colors[colorId];
+        leftCombinationsForColor -= 1;
+        if (leftCombinationsForColor < 1)
+            NewColor();
+        return ret;
+    }
+
+    void NewColor()
+    {
+        if (randomColors)
+        {
+            int tmp;
+            do
+            {
+                tmp = Random.Range(0, colors.Length);
+            } while (tmp == colorId);
+            colorId = tmp;
+        }
+        else
+        {
+            colorId = (colorId + 1) % colors.Length;
+        }
+
+        leftCombinationsForColor = combinationsForColor;
     }
 }
